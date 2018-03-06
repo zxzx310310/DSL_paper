6b879c655d7444350aa6c2b395f3e414ffe3914c
zxzx310310 <jjj84229@gmail.com>
2018-03-06 17:03:38 +0800

    新增選擇商品的參數及程式碼

--- BBP.R
+++ BBP.R
@@ -1,80 +1,98 @@
-sourceData <- read.csv(file = "assets/StoreData.csv")
-goodData <- sourceData
+#----資料初始化----
+sourceData <- read.csv(file = "assets/StoreData.csv") #讀取原始資料
+goodData <- sourceData #將原始資料複製一份
 
-#新增被選擇欄位
-goodData <- cbind(goodData, "Selected" = 0)
+goodData <- cbind(goodData, "Selected" = 0) #新增被選擇欄位
 
 
-#----參數----
+#----環境參數設定----
 maxVolume <- 52*38*28 #最大箱子體積
 maxPrice <- 1200 #最大金額
-categoryList <- c("油", "米", "醬油", "酒", "糖", "麵食", "沖泡", "罐頭", "飲品", "泡麵", "零食") #必需品商品
-#NonRequiredList <- c("飲品", "泡麵", "零食") #非必需品商品
-Vegetarian <- 0 #是否為素食(0為否, 1為是, 3為不限)
+requiredList <- c("油", "米", "醬油", "酒", "鹽", "糖", "麵食") #必需品商品
+nonRequiredList <- c("沖泡", "罐頭", "飲品", "泡麵", "零食") #非必需品商品
+#categoryList <- c("油", "米", "醬油", "酒", "鹽", "糖", "麵食", "沖泡", "罐頭", "飲品", "泡麵", "零食") #必需品商品
+#Vegetarian <- 0 #是否為素食(0為否, 1為是, 3為不限)
 
-canQt <- 2 #罐頭數量
-drinkQt <- 3 #飲料數量
-InstNoodlesQt <- 3 #泡麵數量
-snackQt <- 4 #零食數量
+nonRequiredValues <- 5 #選擇性商品的數量
+#canQt <- 2 #罐頭數量
+#drinkQt <- 3 #飲料數量
+#InstNoodlesQt <- 3 #泡麵數量
+#snackQt <- 4 #零食數量
 
 popAmount <- 100 #人口數量
 
 
-#隨機選擇必要商品(單方執行)
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="油",]), size = 1)
-getIndex <- which(goodData$種類=="油")[requiredGood]
-goodData$Selected[getIndex] <- 1
+#----測試----
+#隨機選擇必要商品(單方執行)-1
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="油",]), size = 1)
+# getIndex <- which(goodData$種類=="油")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="米",]), size = 1)
+# getIndex <- which(goodData$種類=="米")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="醬油",]), size = 1)
+# getIndex <- which(goodData$種類=="醬油")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="酒",]), size = 1)
+# getIndex <- which(goodData$種類=="酒")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="糖",]), size = 1)
+# getIndex <- which(goodData$種類=="糖")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="麵食",]), size = 1)
+# getIndex <- which(goodData$種類=="麵食")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="沖泡",]), size = 1)
+# getIndex <- which(goodData$種類=="沖泡")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# if(Vegetarian==0) {
+#   requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(葷)",]), size = canQt)
+#   getIndex <- which(goodData$種類=="罐頭(葷)")[requiredGood]
+#   goodData$Selected[getIndex] <- 1  
+# } else if(Vegetarian==1) {
+#   requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(素)",]), size = canQt)
+#   getIndex <- which(goodData$種類=="罐頭(素)")[requiredGood]
+#   goodData$Selected[getIndex] <- 1  
+# } else {
+#   requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(素)" | goodData$種類=="罐頭(葷)",]), size = canQt)
+#   getIndex <- which(goodData$種類=="罐頭(素)" | goodData$種類=="罐頭(葷)")[requiredGood]
+#   goodData$Selected[getIndex] <- 1
+# }
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="飲品",]), size = drinkQt)
+# getIndex <- which(goodData$種類=="飲品")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="泡麵",]), size = InstNoodlesQt)
+# getIndex <- which(goodData$種類=="泡麵")[requiredGood]
+# goodData$Selected[getIndex] <- 1
+# 
+# requiredGood <- sample(1:nrow(goodData[goodData$種類=="零食",]), size = snackQt)
+# getIndex <- which(goodData$種類=="零食")[requiredGood]
+# goodData$Selected[getIndex] <- 1
 
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="米",]), size = 1)
-getIndex <- which(goodData$種類=="米")[requiredGood]
-goodData$Selected[getIndex] <- 1
 
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="醬油",]), size = 1)
-getIndex <- which(goodData$種類=="醬油")[requiredGood]
-goodData$Selected[getIndex] <- 1
 
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="酒",]), size = 1)
-getIndex <- which(goodData$種類=="酒")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="糖",]), size = 1)
-getIndex <- which(goodData$種類=="糖")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="麵食",]), size = 1)
-getIndex <- which(goodData$種類=="麵食")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="沖泡",]), size = 1)
-getIndex <- which(goodData$種類=="沖泡")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-if(Vegetarian==0) {
-  requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(葷)",]), size = canQt)
-  getIndex <- which(goodData$種類=="罐頭(葷)")[requiredGood]
-  goodData$Selected[getIndex] <- 1  
-} else if(Vegetarian==1) {
-  requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(素)",]), size = canQt)
-  getIndex <- which(goodData$種類=="罐頭(素)")[requiredGood]
-  goodData$Selected[getIndex] <- 1  
-} else {
-  requiredGood <- sample(1:nrow(goodData[goodData$種類=="罐頭(素)" | goodData$種類=="罐頭(葷)",]), size = canQt)
-  getIndex <- which(goodData$種類=="罐頭(素)" | goodData$種類=="罐頭(葷)")[requiredGood]
-  goodData$Selected[getIndex] <- 1
+#隨機選擇必要商品(單方執行)-2
+for (i in 1:length(requiredList)) {
+  requiredGood <- sample(1:nrow(goodData[goodData$種類==requiredList[i],]), size = 1) #隨機選擇一個每個種類的必要商品
+  getIndex <- which(goodData$種類==requiredList[i])[requiredGood] #抓取被選擇的必要商品
+  goodData$Selected[getIndex] <- 1 #將被抓取的商品從0改為1
 }
 
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="飲品",]), size = drinkQt)
-getIndex <- which(goodData$種類=="飲品")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="泡麵",]), size = InstNoodlesQt)
-getIndex <- which(goodData$種類=="泡麵")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
-requiredGood <- sample(1:nrow(goodData[goodData$種類=="零食",]), size = snackQt)
-getIndex <- which(goodData$種類=="零食")[requiredGood]
-goodData$Selected[getIndex] <- 1
-
+for (i in 1:nonRequiredValues) {
+  categoryGoods <- sample(nonRequiredList, 1)
+  requiredGood <- sample(1:nrow(goodData[goodData$種類==categoryGoods,]), size = 1)
+  getIndex <- which(goodData$種類==categoryGoods)[requiredGood]
+  goodData$Selected[getIndex] <- 1 
+}
 
 
 #檢驗已被選擇的商品
@@ -88,43 +106,62 @@ sum(selectedGood$單價)
 
 #----Function----
 
-#初始人口方法
-initial_pop <- function(good_data, category_list) {
-  for (i in 1:length(category_list)) {
-    #若為罐頭則判斷是否為素食
-    if(category_list[i]=="罐頭") {
-      if(Vegetarian==0) {
-        requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(葷)",]), size = canQt)
-        getIndex <- which(good_data$種類=="罐頭(葷)")[requiredGood]
-        good_data$Selected[getIndex] <- 1  
-      } else if(Vegetarian==1) {
-        requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(素)",]), size = canQt)
-        getIndex <- which(good_data$種類=="罐頭(素)")[requiredGood]
-        good_data$Selected[getIndex] <- 1  
-      } else {
-        requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(素)" | good_data$種類=="罐頭(葷)",]), size = canQt)
-        getIndex <- which(good_data$種類=="罐頭(素)" | good_data$種類=="罐頭(葷)")[requiredGood]
-        good_data$Selected[getIndex] <- 1
-      }
-    } else if(category_list[i]=="飲品") {
-      requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = drinkQt)
-      getIndex <- which(good_data$種類==category_list[i])[requiredGood]
-      good_data$Selected[getIndex] <- 1 
-    } else if(category_list[i]=="泡麵") {
-      requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = InstNoodlesQt)
-      getIndex <- which(good_data$種類==category_list[i])[requiredGood]
-      good_data$Selected[getIndex] <- 1 
-    } else if(category_list[i]=="零食") {  
-      requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = snackQt)
-      getIndex <- which(good_data$種類==category_list[i])[requiredGood]
-      good_data$Selected[getIndex] <- 1 
-    } else {
-      requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = 1)
-      getIndex <- which(good_data$種類==category_list[i])[requiredGood]
-      good_data$Selected[getIndex] <- 1 
-    }
+#初始人口方法(素食選擇)
+# initial_pop <- function(good_data, category_list) {
+#   for (i in 1:length(category_list)) {
+#     #若為罐頭則判斷是否為素食
+#     if(category_list[i]=="罐頭") {
+#       if(Vegetarian==0) {
+#         requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(葷)",]), size = canQt)
+#         getIndex <- which(good_data$種類=="罐頭(葷)")[requiredGood]
+#         good_data$Selected[getIndex] <- 1  
+#       } else if(Vegetarian==1) {
+#         requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(素)",]), size = canQt)
+#         getIndex <- which(good_data$種類=="罐頭(素)")[requiredGood]
+#         good_data$Selected[getIndex] <- 1  
+#       } else {
+#         requiredGood <- sample(1:nrow(good_data[good_data$種類=="罐頭(素)" | good_data$種類=="罐頭(葷)",]), size = canQt)
+#         getIndex <- which(good_data$種類=="罐頭(素)" | good_data$種類=="罐頭(葷)")[requiredGood]
+#         good_data$Selected[getIndex] <- 1
+#       }
+#     } else if(category_list[i]=="飲品") {
+#       requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = drinkQt)
+#       getIndex <- which(good_data$種類==category_list[i])[requiredGood]
+#       good_data$Selected[getIndex] <- 1 
+#     } else if(category_list[i]=="泡麵") {
+#       requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = InstNoodlesQt)
+#       getIndex <- which(good_data$種類==category_list[i])[requiredGood]
+#       good_data$Selected[getIndex] <- 1 
+#     } else if(category_list[i]=="零食") {  
+#       requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = snackQt)
+#       getIndex <- which(good_data$種類==category_list[i])[requiredGood]
+#       good_data$Selected[getIndex] <- 1 
+#     } else {
+#       requiredGood <- sample(1:nrow(good_data[good_data$種類==category_list[i],]), size = 1)
+#       getIndex <- which(good_data$種類==category_list[i])[requiredGood]
+#       good_data$Selected[getIndex] <- 1 
+#     }
+#   }
+#   #print(good_data[good_data$Selected==1,])
+# }
+
+
+#初始人口方法(無素食, 選擇性商品為參數)
+initial_pop <- function(good_data, require_goods, non_require_goods, non_require_values) {
+  for (i in 1:length(require_goods)) {
+    requiredGood <- sample(1:nrow(good_data[good_data$種類==require_goods[i],]), size = 1) #依必要商品的每個類別去隨機挑選出商品
+    getIndex <- which(good_data$種類==require_goods[i])[requiredGood] #取得剛才隨機挑選的商品位置
+    good_data$Selected[getIndex] <- 1 #將被選擇的欄位改為1
   }
-  #print(good_data[good_data$Selected==1,])
+  
+  for (i in 1:non_require_values) {
+    categoryGoods <- sample(non_require_goods, 1) #隨機挑選選擇性商品的類別
+    requiredGood <- sample(1:nrow(good_data[good_data$種類==categoryGoods,]), size = 1) #依選擇性商品的類別去隨機挑選出商品
+    getIndex <- which(good_data$種類==categoryGoods)[requiredGood] #取得剛才隨機挑選的商品位置
+    good_data$Selected[getIndex] <- 1 #將被選擇的欄位改為1
+  }
+  selectedGood <- good_data[good_data$Selected==1,] #將所有被選擇的欄位為1的資料拉出
+  return(selectedGood) #回傳結果
 }
 
 
@@ -150,7 +187,7 @@ cross_over <- function() {
 #產生初始口(遵照popAmount數量)
 geneList <- list()
 for (i in 1:popAmount) {
-  gene <- initial_pop(good_data = goodData, category_list = categoryList)
+  gene <- initial_pop(good_data = goodData, require_goods = requiredList, non_require_goods = nonRequiredList, non_require_values = nonRequiredValues)
   geneList[[i]] <- list(gene)
 }
 
